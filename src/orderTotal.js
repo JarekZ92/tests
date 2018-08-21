accb804bef4a75c5ac05cdd3d82946f487b7ecf4
Jarek <z.jarek92@gmail.com>
2018-08-21 13:03:11 +0200

    test passed

--- src/orderTotal.js
+++ src/orderTotal.js
@@ -1,3 +1,10 @@
 export const orderTotal = (cart) => (
-    cart.reduce((r, el) => r + el.price, 0)
+    cart.reduce(
+        (r, el) => {
+            const realQuantity = el.quantity === undefined ? 1 : el.quantity
+
+            return r + (el.price * realQuantity)
+        },
+        0
+    )
 )
--- src/orderTotal.test.js
+++ src/orderTotal.test.js
@@ -1,15 +1,33 @@
 import { orderTotal } from './orderTotal'
 
-const cart = [
+const cart0 = []
+
+const cart1 = [
     { name: 'Wódka', price: 25 },
     { name: 'Wino', price: 15 },
     { name: 'Denaturat', price: 5 },
     { name: 'Bułka', price: 0.5 }
 ]
 
-//total 45,5
+const cart2 = [
+    { name: 'Wódka', price: 25, quantity: 1 },
+    { name: 'Wino', price: 15, quantity: 2}
+]
 
-//test
-if (orderTotal(cart) !== 45.5) {
-    throw new Error('Cant calculate order total')
-}
+const cart3 = [
+    { name: 'Wódka', price: 25, quantity: 1 },
+    { name: 'Wino', price: 15, quantity: 2}
+]
+//test in JEST
+
+test('can calculate order total', () => {
+    expect(orderTotal(cart0)).toBe(0)
+})
+
+test('Cant calculate order total', () => {
+    expect(orderTotal(cart1)).toBe(45.5)
+})
+
+test('Cant calculate order total with quantity', () => {
+    expect(orderTotal(cart2)).toBe(55)
+})
